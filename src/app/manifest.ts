433dd5a0dadb163d0e4614a036f440b95f2a11e1
Ashish Upadhyay <ashishup1999@gmail.com>
2024-03-28 22:45:41 +0530

    manifest file added

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Codekar",
+    short_name: "CK",
+    description: "Codekar befikr!!!",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/logoWhite.svg",
+        sizes: "192x192",
+        type: "image/svg+xml",
+      },
+    ],
+  };
+}
